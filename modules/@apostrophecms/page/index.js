6eab0c1102ee29fdd0dd99b0d9d5720348329bed
Broc Gailit <brocgailit@gmail.com>
2022-02-05 15:03:26 -0800

    first commit

---
+++ modules/@apostrophecms/page/index.js
@@ -0,0 +1,13 @@
+// This configures the @apostrophecms/pages module to add a "home" page type to the
+// pages menu
+
+module.exports = {
+  options: {
+    types: [
+      {
+        name: 'default-page',
+        label: 'Coming Soon'
+      }
+    ]
+  }
+};
